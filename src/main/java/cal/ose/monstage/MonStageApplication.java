c402ea9db6972e3602ace7c32528464e14409eae
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-09-07 13:51:43 -0400

    Créé le projet

---
+++ src/main/java/cal/ose/monstage/MonStageApplication.java
@@ -0,0 +1,11 @@
+package cal.ose.monstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonStageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonStageApplication.class, args);
+    }
+}
---
+++ src/test/java/cal/ose/monstage/MonStageApplicationTests.java
@@ -0,0 +1,11 @@
+package cal.ose.monstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonStageApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
