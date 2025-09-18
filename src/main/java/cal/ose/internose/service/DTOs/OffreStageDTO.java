0a10df8e94a684692c6ea9ea1c2483fc238517f8
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-09-18 13:57:57 -0400

    Ajouté une classe contrôleur pour l'employeur et quelques méthodes supplémentaires (EQ6-39)

--- src/main/java/cal/ose/internose/service/DTOs/OffreStageDTO.java
+++ src/main/java/cal/ose/internose/service/DTOs/OffreStageDTO.java
@@ -1,9 +1,11 @@
 package cal.ose.internose.service.DTOs;
 
+import cal.ose.internose.modele.OffreStage;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Builder
 @Getter
@@ -13,6 +15,24 @@ public class OffreStageDTO {
     private String competencesRequises;
     private int duree;
     private LocalDate dateDebut;
+    private LocalDate dateFin;
     private double remuneration;
     private String adresse;
+
+    public static OffreStageDTO fromEntity(OffreStage offreStage) {
+        return OffreStageDTO.builder()
+            .titrePoste(offreStage.getTitrePoste())
+            .descriptionTaches(offreStage.getDescriptionTaches())
+            .competencesRequises(offreStage.getCompetencesRequises())
+            .duree(offreStage.getDuree())
+            .dateDebut(offreStage.getDateDebut())
+            .dateFin(offreStage.getDateFin())
+            .remuneration(offreStage.getRemuneration())
+            .adresse(offreStage.getAdresse())
+            .build();
+    }
+
+    public static List<OffreStageDTO> fromEntityList(List<OffreStage> offreStages) {
+        return offreStages.stream().map(OffreStageDTO::fromEntity).toList();
+    }
 }
--- src/main/java/cal/ose/internose/service/EmployeurService.java
+++ src/main/java/cal/ose/internose/service/EmployeurService.java
@@ -3,14 +3,22 @@ package cal.ose.internose.service;
 import cal.ose.internose.modele.OffreStage;
 import cal.ose.internose.persistance.OffreStageDAO;
 import cal.ose.internose.service.DTOs.OffreStageDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Transactional
 public class EmployeurService {
     private final OffreStageDAO offreStageDAO;
 
     public EmployeurService(OffreStageDAO offreStageDAO) {
         this.offreStageDAO = offreStageDAO;
+    }
+
+    public List<OffreStageDTO> listerOffresStage() {
+        return OffreStageDTO.fromEntityList(offreStageDAO.findAll());
     }
 
     public void creerOffreStage(OffreStageDTO offreStageDTO) {
