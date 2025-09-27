1db856ff5b4ab8238c004bb842c7ef28400d421a
rodri <rodriguezquirozd10@gmail.com>
2025-09-27 19:25:53 -0400

    Ajout de la fonction de filtrage findInternshipsBy() dans InternshipOfferDAO

--- src/main/java/cal/ose/internose/persistance/InternshipOfferDAO.java
+++ src/main/java/cal/ose/internose/persistance/InternshipOfferDAO.java
@@ -3,11 +3,21 @@ package cal.ose.internose.persistance;
 import cal.ose.internose.modele.Employer;
 import cal.ose.internose.modele.InternshipOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-@Repository
 public interface InternshipOfferDAO extends JpaRepository<InternshipOffer, Long> {
     List<InternshipOffer> findAllByEmployer(Employer employer);
+
+    @Query(
+            "SELECT io FROM InternshipOffer io " +
+            "WHERE (:domain IS NULL OR io.domain = :domain) " +
+            "AND (:valid IS NULL OR io.validee = :valid) " +
+            "AND (:enterprise IS NULL OR io.employer.enterprise = :enterprise)"
+    )
+    List<InternshipOffer> findInternshipsBy(@Param("domain") String domain,
+                                            @Param("valid") Boolean valid,
+                                            @Param("enterprise") String enterprise);
 }
