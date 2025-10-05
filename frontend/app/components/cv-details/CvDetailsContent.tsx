eec673833c51582eede68acc178ea7a83d6b4e60
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-10-05 17:48:51 -0400

    Ajouté l'insertion des données dans CommandLineRunner pour faciliter les revues et les démos

--- frontend/app/components/cv-details/CvDetailsContent.tsx
+++ frontend/app/components/cv-details/CvDetailsContent.tsx
@@ -110,7 +110,7 @@ export function CvDetailsContent() {
   if (!error && !loading && cvDetails)
     return (
         <>
-            <div onClick={() => {navigate("/im-dashboard")}} className="flex items-center gap-2 w-[10%] bg-red-600 text-white px-3 py-2 rounded cursor-pointer hover:bg-red-700 transition">
+            <div onClick={() => {navigate("/im-dashboard")}} className="flex items-center gap-2 w-[10%] bg-red-600 text-white px-3 py-2 rounded cursor-pointer hover:bg-red-700 transition mt-3 ml-3">
                 <svg
                     className="w-4 h-4"
                     fill="none"
