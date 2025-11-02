901806a0d7681c1433fdd317374a5b03364839ce
rodri <rodriguezquirozd10@gmail.com>
2025-11-01 20:07:34 -0400

    EQ6-82 Ajout d'un modal de confirmation lorsque l'etudiant veut accepter ou rejeter une offre de stage

--- frontend/src/components/dashboard/ApplicationsOfStudent.tsx
+++ frontend/src/components/dashboard/ApplicationsOfStudent.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import type {StudentApplication} from "~/interfaces";
+import ApplicationConfirmationModal from "./ApplicationConfirmationModal";
 
 interface ApplicationsOfStudentProps {
     applications: StudentApplication[]
@@ -8,6 +9,8 @@ interface ApplicationsOfStudentProps {
 
 export default function ApplicationsOfStudent({applications}: ApplicationsOfStudentProps) {
     const { t } = useTranslation();
+    const [applicationToConfirm, setApplicationToConfirm] = useState<StudentApplication>()
+    const [confirmationType, setConfirmationType] = useState<'REJECT' | 'ACCEPT'>('REJECT')
 
     const getApplicationDate = (application: StudentApplication) => {
         const isoString = "2025-11-01T18:06:37.31557";
@@ -39,72 +42,79 @@ export default function ApplicationsOfStudent({applications}: ApplicationsOfStud
       };
 
     return (
-        <div className="bg-white rounded-lg shadow-md pb-6">
-            <div className="px-6 py-4">
-                <h2 className="text-xl font-semibold text-gray-900">Mes candidatures</h2>
-            </div>
-    
-          <div className="divide-y divide-gray-200 shadow-lg rounded-lg ms-6 me-6">
-            {applications.map((application, index) => (
-              <div 
-                className={`p-6 hover:bg-gray-50 transition-colors`}
-                key={index}
-              >
-                <div className="flex justify-between items-start">
-                  <div className="flex-1">
-                    <div className="flex items-start justify-between">
-                      <div className="flex-1">
-                        <h3 className="text-lg font-semibold text-gray-900 mb-2">{application.internshipOfferTitle}</h3>
-                        <div className="flex flex-wrap items-center gap-4 text-sm text-gray-600">
-                          <div className="flex items-center">
-                            <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>
-                            {application.internshipOfferAddress}
-                          </div>
-                          <div className="flex items-center">
-                            <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            {application.internshipOfferStartDate} - {application.internshipOfferEndDate}
-                          </div>
-                          <div className="flex items-center">
-                            <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            {application.internshipOfferDuration} {t('internship.weeks')}
-                          </div>
+        <>
+            <div className="bg-white rounded-lg shadow-md pb-6">
+                <div className="px-6 py-4">
+                    <h2 className="text-xl font-semibold text-gray-900">Mes candidatures</h2>
+                </div>
+        
+            <div className="divide-y divide-gray-200 shadow-lg rounded-lg ms-6 me-6">
+                {applications.map((application, index) => (
+                <div 
+                    className={`p-6 hover:bg-gray-50 transition-colors`}
+                    key={index}
+                >
+                    <div className="flex justify-between items-start">
+                    <div className="flex-1">
+                        <div className="flex items-start justify-between">
+                        <div className="flex-1">
+                            <h3 className="text-lg font-semibold text-gray-900 mb-2">{application.internshipOfferTitle}</h3>
+                            <div className="flex flex-wrap items-center gap-4 text-sm text-gray-600">
+                            <div className="flex items-center">
+                                <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                {application.internshipOfferAddress}
+                            </div>
+                            <div className="flex items-center">
+                                <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                </svg>
+                                {application.internshipOfferStartDate} - {application.internshipOfferEndDate}
+                            </div>
+                            <div className="flex items-center">
+                                <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                                {application.internshipOfferDuration} {t('internship.weeks')}
+                            </div>
+                            </div>
                         </div>
-                      </div>
-                      
-                        <div className="ml-4 flex flex-col items-end space-y-2">
-                            {getStatusBadge(application)}
-                        </div>
+                        
+                            <div className="ml-4 flex flex-col items-end space-y-2">
+                                {getStatusBadge(application)}
+                            </div>
 
+                        </div>
                     </div>
-                  </div>
-                </div>
-                <div className="mt-4 space-y-3 flex">
-                  <div>
-                    <h4 className="text-sm font-medium text-gray-900 mb-1">{t('employer.date')}</h4>
-                    <p className="text-sm text-gray-700 leading-relaxed">{getApplicationDate(application)}</p>
-                  </div>
+                    </div>
+                    <div className="mt-4 space-y-3 flex">
+                    <div>
+                        <h4 className="text-sm font-medium text-gray-900 mb-1">{t('employer.date')}</h4>
+                        <p className="text-sm text-gray-700 leading-relaxed">{getApplicationDate(application)}</p>
+                    </div>
 
-                  {// application.applicationStatus === "APPROVED" && 
-                  <div className="flex gap-1 mt-4 ms-auto text-sm">
-                    <button className="rounded-sm px-2 bg-green-500 text-white hover:bg-green-600 hover:cursor-pointer">
-                        Accepter
-                    </button>
-                    <button className="rounded-sm px-2 bg-red-500 text-white hover:bg-red-600 hover:cursor-pointer">
-                        Refuser
-                    </button>
-                  </div>}
+                    {// application.applicationStatus === "APPROVED" && 
+                    <div className="flex gap-1 mt-4 ms-auto text-sm">
+                        <button onClick={() => {setApplicationToConfirm(application); setConfirmationType('ACCEPT')}} className="rounded-sm px-2 bg-green-500 text-white hover:bg-green-600 hover:cursor-pointer">
+                            Accepter
+                        </button>
+                        <button onClick={() => {setApplicationToConfirm(application); setConfirmationType('REJECT')}} className="rounded-sm px-2 bg-red-500 text-white hover:bg-red-600 hover:cursor-pointer">
+                            Refuser
+                        </button>
+                    </div>}
+                    </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </div>
+                ))}
+            </div>
+            </div>
+
+            {applicationToConfirm && <ApplicationConfirmationModal 
+                    application={applicationToConfirm} 
+                    mode={confirmationType}
+                    onClose={() => setApplicationToConfirm(undefined)}></ApplicationConfirmationModal>}
+        </>
       );
 }
 
