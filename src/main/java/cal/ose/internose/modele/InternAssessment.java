5f8d3f3e3484710780cd9707ff42560a709bd4b4
Artyom Markaryan <artyommarkaryan29@gmail.com>
2025-11-11 20:41:00 -0500

    Ajouté les classes InternAssessment et InternAssessmentDTO (EQ6-99)

---
+++ src/main/java/cal/ose/internose/modele/InternAssessment.java
@@ -0,0 +1,123 @@
+﻿package cal.ose.internose.modele;
+
+import cal.ose.internose.service.DTOs.InternAssessmentDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * Fiche d'évaluation du stagiaire
+ */
+@Entity
+@Table(name = "INTERN_ASSESSMENTS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class InternAssessment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String studentName;
+    private String studentProgram;
+    private String companyName;
+    private String supervisorName;
+    private String supervisorTitle;
+    private String supervisorPhoneNumber;
+
+    /**
+     * Les réponses possibles aux questions à choix multiples
+     */
+    public enum AssessmentOptions {
+        COMPLETELY_AGREE,
+        PARTIALLY_AGREE,
+        PARTIALLY_DISAGREE,
+        COMPLETELY_DISAGREE,
+        NOT_APPLICABLE
+    }
+    /**
+     * Ce dictionnaire contient les réponses aux questions à choix multiples
+     */
+    @ElementCollection
+    @CollectionTable(name = "INTERN_ASSESSMENT_MAPS", joinColumns = @JoinColumn(name = "id"))
+    @MapKeyColumn(name = "intern_assessment_key")
+    @Column(name = "intern_assessment_value")
+    @Enumerated(EnumType.STRING)
+    private Map<String, AssessmentOptions> internAssessment;
+    /**
+     * Ce dictionnaire contient les commentaires à la fin de chaque section des questions à choix multiples
+     */
+    @ElementCollection
+    @CollectionTable(name = "COMMENT_MAPS", joinColumns = @JoinColumn(name = "id"))
+    @MapKeyColumn(name = "intern_assessment_comment_key")
+    @Column(name = "intern_assessment_comment_value")
+    @Enumerated(EnumType.STRING)
+    private Map<String, String> internAssessmentComments;
+
+    /**
+     * Appréciation globale du stagiaire
+     */
+    public enum OverallInternAppreciation {
+        GREATLY_EXCEEDS_EXPECTATIONS,
+        EXCEEDS_EXPECTATIONS,
+        FULLY_MEETS_EXPECTATIONS,
+        PARTIALLY_MEETS_EXPECTATIONS,
+        DOES_NOT_MEET_EXPECTATIONS
+    }
+    private OverallInternAppreciation overallInternAppreciation;
+    /**
+     * Précisez votre appreciation
+     */
+    private String appreciationComment;
+    /**
+     * Cette évaluation a été discutée avec le stagiaire?
+     */
+    private boolean discussedWithTheIntern;
+
+    /**
+     * Le nombre d'heures réel par semaine d'encadrement accordé au stagiaire
+     */
+    private double weeklySupervisionHours;
+
+    /**
+     * L'entreprise aimerait accueillir cet étudiant pour son prochain stage?
+     */
+    public enum FutureCollaboration { YES, MAYBE, NO }
+    private FutureCollaboration futureCollaboration;
+    /**
+     * La formation technique du stagiaire était-elle suffisante pour accomplir le mandat de stage?
+     */
+    private String academicPreparationAdequacy;
+
+    private String signerName;
+    private String signerTitle;
+    private String signature;
+    private String signatureDate;
+
+    private static InternAssessment fromDTO(InternAssessmentDTO internAssessmentDTO) {
+        return InternAssessment.builder()
+            .studentName(internAssessmentDTO.getStudentName())
+            .studentProgram(internAssessmentDTO.getStudentProgram())
+            .companyName(internAssessmentDTO.getCompanyName())
+            .supervisorName(internAssessmentDTO.getSupervisorName())
+            .supervisorTitle(internAssessmentDTO.getSupervisorTitle())
+            .supervisorPhoneNumber(internAssessmentDTO.getSupervisorPhoneNumber())
+            .internAssessment(internAssessmentDTO.getInternAssessment())
+            .internAssessmentComments(internAssessmentDTO.getInternAssessmentComments())
+            .overallInternAppreciation(internAssessmentDTO.getOverallInternAppreciation())
+            .discussedWithTheIntern(internAssessmentDTO.isDiscussedWithTheIntern())
+            .weeklySupervisionHours(internAssessmentDTO.getWeeklySupervisionHours())
+            .futureCollaboration(internAssessmentDTO.getFutureCollaboration())
+            .academicPreparationAdequacy(internAssessmentDTO.getAcademicPreparationAdequacy())
+            .signerName(internAssessmentDTO.getSignerName())
+            .signerTitle(internAssessmentDTO.getSignerTitle())
+            .signature(internAssessmentDTO.getSignature())
+            .signatureDate(internAssessmentDTO.getSignatureDate())
+            .build();
+    }
+}
---
+++ src/main/java/cal/ose/internose/service/DTOs/InternAssessmentDTO.java
@@ -0,0 +1,64 @@
+﻿package cal.ose.internose.service.DTOs;
+
+import cal.ose.internose.modele.InternAssessment;
+import cal.ose.internose.modele.InternAssessment.AssessmentOptions;
+import cal.ose.internose.modele.InternAssessment.FutureCollaboration;
+import cal.ose.internose.modele.InternAssessment.OverallInternAppreciation;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+/**
+ * La classe DTO pour l'objet {@link InternAssessment}
+ */
+@Builder
+@Getter
+public class InternAssessmentDTO {
+    private String studentName;
+    private String studentProgram;
+    private String companyName;
+    private String supervisorName;
+    private String supervisorTitle;
+    private String supervisorPhoneNumber;
+
+    private Map<String, AssessmentOptions> internAssessment;
+    private Map<String, String> internAssessmentComments;
+
+    private OverallInternAppreciation overallInternAppreciation;
+    private String appreciationComment;
+    private boolean discussedWithTheIntern;
+
+    private double weeklySupervisionHours;
+
+    private FutureCollaboration futureCollaboration;
+    private String academicPreparationAdequacy;
+
+    private String signerName;
+    private String signerTitle;
+    private String signature;
+    private String signatureDate;
+
+    private static InternAssessmentDTO fromEntity(InternAssessment internAssessment) {
+        return InternAssessmentDTO.builder()
+            .studentName(internAssessment.getStudentName())
+            .studentProgram(internAssessment.getStudentProgram())
+            .companyName(internAssessment.getCompanyName())
+            .supervisorName(internAssessment.getSupervisorName())
+            .supervisorTitle(internAssessment.getSupervisorTitle())
+            .supervisorPhoneNumber(internAssessment.getSupervisorPhoneNumber())
+            .internAssessment(internAssessment.getInternAssessment())
+            .internAssessmentComments(internAssessment.getInternAssessmentComments())
+            .overallInternAppreciation(internAssessment.getOverallInternAppreciation())
+            .appreciationComment(internAssessment.getAppreciationComment())
+            .discussedWithTheIntern(internAssessment.isDiscussedWithTheIntern())
+            .weeklySupervisionHours(internAssessment.getWeeklySupervisionHours())
+            .futureCollaboration(internAssessment.getFutureCollaboration())
+            .academicPreparationAdequacy(internAssessment.getAcademicPreparationAdequacy())
+            .signerName(internAssessment.getSignerName())
+            .signerTitle(internAssessment.getSignerTitle())
+            .signature(internAssessment.getSignature())
+            .signatureDate(internAssessment.getSignatureDate())
+            .build();
+    }
+}
