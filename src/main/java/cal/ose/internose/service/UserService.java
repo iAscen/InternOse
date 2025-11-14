a1322a6e8d818b0fcf1fd7f2440184ff5de9e189
rodri <rodriguezquirozd10@gmail.com>
2025-11-13 22:08:36 -0500

    Ajout de getSessionOfAuthenticatedUser() dans UserService

--- src/main/java/cal/ose/internose/service/UserService.java
+++ src/main/java/cal/ose/internose/service/UserService.java
@@ -19,6 +19,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -131,6 +132,14 @@ public class UserService {
 
         user.setSession(session);
         userDAO.save(user);
+    }
+
+    public String getSessionOfAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Credentials credentials = (Credentials) auth.getCredentials();
+        User user = userDAO.findByCredentials_Email(credentials.getEmail()).orElseThrow();
+
+        return user.getSession();
     }
 
     public void verifyPasswordCriteria(String password) throws WeakPasswordException {
