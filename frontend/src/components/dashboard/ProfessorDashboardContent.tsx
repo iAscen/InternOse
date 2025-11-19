755343c37fde9ed785c8d08a98fd93bce563e430
rodri <rodriguezquirozd10@gmail.com>
2025-11-18 19:58:23 -0500

    EQ6-103 Ajout des tests dans ProfessorDashboardContent.test.tsx

--- frontend/src/components/dashboard/ProfessorDashboardContent.tsx
+++ frontend/src/components/dashboard/ProfessorDashboardContent.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 import type { InternshipContract } from "~/interfaces";
 import DashboardSidebar from "./DashboardSidebar";
 import { professorAPI } from "../../services/ProfessorAPI"
